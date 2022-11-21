ec7ca82c788bd22c3376086b2228ee8a01263d54
MrNinjaPowha <joakim.gietl@gmail.com>
2022-11-21 12:22:35 +0100

    Changed name and functionality of fuel upgrades

--- src/js/productionHandler.js
+++ src/js/productionHandler.js
@@ -1,19 +1,19 @@
 export default class ProductionHandler {
   multipliers = {
-    all: 0,
+    fuel: 0,
     clicker: 1,
     buildings: 1,
   };
 
   getProduction(type) {
-    return this.multipliers.all * this.multipliers[type];
+    return this.multipliers.fuel * this.multipliers[type];
   }
 
   newUpgrade(upgrade) {
-    if (upgrade.effects[0].type === 'start') this.multipliers.all = 100;
-
     upgrade.effects.forEach((effect) => {
-      if (this.multipliers.hasOwnProperty(effect.type))
+      if (effect.type === 'start') this.multipliers.fuel = 1;
+      if (effect.type === 'fuel') this.multipliers.fuel = effect.value;
+      else if (this.multipliers.hasOwnProperty(effect.type))
         this.multipliers[effect.type] *= effect.value;
     });
   }
