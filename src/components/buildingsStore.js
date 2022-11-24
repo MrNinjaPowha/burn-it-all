87e26d6780f47510cfeb0ae5c9e267fcfcd70707
MrNinjaPowha <joakim.gietl@gmail.com>
2022-11-24 15:02:00 +0100

    Reformatted button appereance

--- src/components/buildingsStore.js
+++ src/components/buildingsStore.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import currentCost from '../js/calculators';
+import formatNumber from '../js/formatters';
 
 export default class BuildingStore extends React.Component {
   render() {
@@ -9,18 +11,18 @@ export default class BuildingStore extends React.Component {
         buildings.push(
           <li key={key}>
             <button
-              className="flex w-full items-center justify-between gap-2 rounded bg-gray-200 p-2"
+              className="flex max-h-min w-full items-center justify-between gap-2 rounded bg-gray-200 p-2"
               onClick={() => this.props.onPurchase(key)}
             >
-              <div className="flex gap-2">
+              <div className="flex items-center gap-2">
                 <img
-                  className="aspect-square h-6 w-6 overflow-hidden"
+                  className="aspect-square w-16 overflow-hidden"
                   src={`${process.env.PUBLIC_URL}/images/buildings/${values.image}`}
                   alt={key}
                 />
-                <div>
-                  <h4 className="text-xl">{this.props.lang[key] || key}</h4>
-                  <span>{values.cost}</span>
+                <div className="flex flex-col items-start">
+                  <h4 className="text-2xl">{this.props.lang[key] || key}</h4>
+                  <span>{formatNumber(currentCost(values.cost, values.count))}</span>
                 </div>
               </div>
               <span className="text-3xl">{values.count}</span>
