0ab812a9f13915e96b957dfc4b72089dd2e8050b
MrNinjaPowha <you@example.com>
2022-11-25 17:51:11 +0100

    Added overflow hidden to image alt

--- src/components/upgrade.js
+++ src/components/upgrade.js
@@ -65,6 +65,7 @@ export default class Upgrade extends React.Component {
           onBlur={this.onFocusOut}
         >
           <img
+            className="overflow-hidden"
             src={`${process.env.PUBLIC_URL}/images/upgrades/${this.props.data.image}`}
             alt={this.props.data.name}
           />
