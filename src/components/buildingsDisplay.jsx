148e7d518d2d8906c31fccef29c54cca6dd727f3
MrNinjaPowha <joakim.gietl@gmail.com>
2022-11-30 15:40:50 +0100

    Added image generation for building display

--- src/components/buildingsDisplay.jsx
+++ src/components/buildingsDisplay.jsx
@@ -10,7 +10,7 @@ export default class BuildingsDisplay extends React.Component {
           <li key={key}>
             <DisplayFrame
               name={key}
-              count={values.count}
+              data={values}
               upgrades={this.props.upgrades}
               lang={this.props.lang}
             />
@@ -24,10 +24,51 @@ export default class BuildingsDisplay extends React.Component {
 }
 
 class DisplayFrame extends React.Component {
+  rowsPerSize = {
+    small: 4,
+    medium: 3,
+    large: 2,
+  };
+
+  renderRow(currentRow) {
+    let images = [];
+
+    for (
+      let i = currentRow;
+      i < this.props.data.count;
+      i += this.rowsPerSize[this.props.data.size]
+    ) {
+      images.push(
+        <li>
+          <img
+            className="object-contain"
+            key={`img${i}`}
+            src={`${process.env.PUBLIC_URL}/images/buildings/${this.props.data.image}`}
+            alt={this.props.name}
+          />
+        </li>
+      );
+    }
+
+    return (
+      <ul className="flex max-w-full gap-1" key={`row${currentRow}`}>
+        {images}
+      </ul>
+    );
+  }
+
   render() {
+    let rows = [];
+
+    for (let i = 0; i < this.rowsPerSize[this.props.data.size]; i++) {
+      rows.push(<li>{this.renderRow(i)}</li>);
+    }
+
     return (
       <>
-        <div className="">{this.props.name}</div>
+        <ul className={`display-bg-${this.props.data.background} flex max-w-full flex-col gap-1`}>
+          {rows}
+        </ul>
         <div className="bg-warning-pattern-horizontal h-4 w-full" />
       </>
     );
