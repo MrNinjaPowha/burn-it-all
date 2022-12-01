d003997631c4e30aa76246dab5002654a87742bf
MrNinjaPowha <joakim.gietl@gmail.com>
2022-12-01 16:26:09 +0100

    Changed buildings display to a canvas

--- src/components/buildingsDisplay.jsx
+++ src/components/buildingsDisplay.jsx
@@ -7,13 +7,9 @@ export default class BuildingsDisplay extends React.Component {
     for (const [key, values] of Object.entries(this.props.buildings)) {
       if (values.count) {
         frames.push(
-          <li key={key}>
-            <DisplayFrame
-              name={key}
-              data={values}
-              upgrades={this.props.upgrades}
-              lang={this.props.lang}
-            />
+          <li key={key} className="h-32">
+            <DisplayCanvas id={key} data={values} />
+            <div className="bg-warning-pattern-horizontal h-4" />
           </li>
         );
       }
@@ -23,56 +19,8 @@ export default class BuildingsDisplay extends React.Component {
   }
 }
 
-class DisplayFrame extends React.Component {
-  rowsPerSize = {
-    small: 4,
-    medium: 3,
-    large: 2,
-  };
-
-  renderRow(currentRow) {
-    let images = [];
-
-    for (
-      let i = currentRow;
-      i < this.props.data.count;
-      i += this.rowsPerSize[this.props.data.size]
-    ) {
-      images.push(
-        <li>
-          <img
-            className="object-contain"
-            key={`img${i}`}
-            src={`${process.env.PUBLIC_URL}/images/buildings/${this.props.data.image}`}
-            alt={this.props.name}
-          />
-        </li>
-      );
-    }
-
-    return (
-      <ul className="flex gap-1" key={`row${currentRow}`}>
-        {images}
-      </ul>
-    );
-  }
-
+class DisplayCanvas extends React.Component {
   render() {
-    let rows = [];
-
-    for (let i = 0; i < this.rowsPerSize[this.props.data.size]; i++) {
-      rows.push(<li>{this.renderRow(i)}</li>);
-    }
-
-    return (
-      <>
-        <ul
-          className={`display-bg-${this.props.data.background} flex h-24 flex-col justify-evenly gap-1`}
-        >
-          {rows}
-        </ul>
-        <div className="bg-warning-pattern-horizontal h-4 w-full" />
-      </>
-    );
+    return <canvas id={this.props.id} />;
   }
 }
