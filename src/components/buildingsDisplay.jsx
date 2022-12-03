d69ce4b091f8aa1dd68ddf34f39bd16e5c75f4fe
MrNinjaPowha <you@example.com>
2022-12-03 19:33:54 +0100

    Created a function to generate and place images

--- src/components/buildingsDisplay.jsx
+++ src/components/buildingsDisplay.jsx
@@ -7,8 +7,8 @@ export default class BuildingsDisplay extends React.Component {
     for (const [key, values] of Object.entries(this.props.buildings)) {
       if (values.count) {
         frames.push(
-          <li key={key} className="h-32">
-            <DisplayCanvas id={key} data={values} />
+          <li key={key}>
+            <DisplayCanvas name={key} data={values} />
             <div className="bg-warning-pattern-horizontal h-4" />
           </li>
         );
@@ -20,7 +20,53 @@ export default class BuildingsDisplay extends React.Component {
 }
 
 class DisplayCanvas extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.frame = null;
+    this.imageRefs = [];
+
+    this.setFrame = (element) => (this.frame = element);
+  }
+
+  renderImages() {
+    let images = [];
+
+    for (let i = 0; i < this.props.data.count; i++) {
+      const style = {
+        position: 'absolute',
+        height: `${100 * this.props.data.display.size}%`,
+        width: 'auto',
+        bottom: `${100 - 100 * this.props.data.display.rows[i % 3] + 5 * Math.random()}%`,
+        left: `${Math.floor(i / 3) * 30 + 5 * Math.random()}px`,
+        zIndex: i % 3,
+      };
+
+      if (this.imageRefs[i]) {
+        // Prevents rerandomization of image positions
+        style.left = this.imageRefs[i].style.left;
+        style.bottom = this.imageRefs[i].style.bottom;
+      }
+
+      images.push(
+        <img
+          style={style}
+          key={i}
+          ref={(ref) => (this.imageRefs[i] = ref)}
+          src={`${process.env.PUBLIC_URL}/images/buildings/${this.props.data.image}`}
+          alt={this.props.name}
+        />
+      );
+    }
+
+    return images;
+  }
+
   render() {
-    return <canvas id={this.props.id} />;
+    return (
+      <div className="relative h-28 w-full overflow-hidden" ref={this.setFrame}>
+        {this.renderImages()}
+      </div>
+    );
   }
 }
