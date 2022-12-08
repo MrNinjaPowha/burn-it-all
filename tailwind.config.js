1891f526f57095a81f45fa4755ae985a4bc89d3c
MrNinjaPowha <joakim.gietl@gmail.com>
2022-12-08 15:58:59 +0100

    Added large inner shadow to store and display

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       minWidth: {
         4: '1rem',
       },
+      boxShadow: {
+        'inner-dark': 'inset 0 0 4rem 1rem rgb(0 0 0 / 0.5)',
+      },
     },
   },
   plugins: [],
